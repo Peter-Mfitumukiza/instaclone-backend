578543efe7495ffc1aa7910339fd31e2b6108aa2
Peter Mfitumukiza <mfitep6@gmail.com>
2022-01-13 16:09:10 +0200

    testing user and auth controllers

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended: false}));
 app.get("/test", (req,res)=>{
     return res.json({ status: "success", message: "we are ready to move Peter!!" });
 });
-// app.use("/user", userController);
-// app.use("/auth", auth);
+app.use("/user", userController);
+app.use("/auth", auth);
 
 app.listen(3000, ()=>console.log("Server running on port 3000"));
