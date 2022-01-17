bdc90f1ac662aeb0b8566f03e5043cb003552e86
Peter Mfitumukiza <mfitep6@gmail.com>
2022-01-17 09:46:38 +0200

    checking connection to atlas

--- models/mongod.js
+++ models/mongod.js
@@ -3,5 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DB_CONN)
     .then(()=>console.log("DB connected successfully..."))
-    .catch(err=>console.error(err))
+    .catch(err=> {
+        console.error("An error occurred while connecting:", err);
+    })
     
